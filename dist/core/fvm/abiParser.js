3e1f63eccd28926030317fc4ef2a0ad9f97e00b2
Denise Cotton <algorythmai42@gmail.com>
2026-04-17 06:44:29 -0500

    Delete dist/core/fvm directory

--- dist/core/fvm/abiParser.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.extractFvmEvents = extractFvmEvents;
-function extractFvmEvents(abi) {
-    return abi.filter((item) => item.type === 'event');
-}
