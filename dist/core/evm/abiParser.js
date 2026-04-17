3c95d662ecc1c22c4113be0aef226c36b796e835
Denise Cotton <algorythmai42@gmail.com>
2026-04-17 06:43:56 -0500

    Delete dist/core/evm directory

--- dist/core/evm/abiParser.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.extractEvents = extractEvents;
-function extractEvents(abi) {
-    return abi.filter((item) => item.type === 'event');
-}
